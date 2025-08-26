c8eb7461f697cb815e9c443f1ed756fcbbbc29c7
templateshed <seajay.abbott@gmail.com>
2025-08-26 13:13:38 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import ErrorBoundary from "@/components/common/ErrorBoundary";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
